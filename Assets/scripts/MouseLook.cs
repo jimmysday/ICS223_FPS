b5dcfbbc7d2fd3d8299baf34d53dfd19ffe03c26
jimmysday <jaminzhang99@gmail.com>
2025-01-27 23:33:46 -0800

    adding mouse input

---
+++ Assets/scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Mouselook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseXAndY,
+        MouseX,
+        MouseY
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
