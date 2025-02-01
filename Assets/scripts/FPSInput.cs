284a145e51edeec97af90e4c036b18e91bb0ec0c
jimmysday <jaminzhang99@gmail.com>
2025-01-31 21:14:47 -0800

    task 2 completed

--- Assets/scripts/FPSInput.cs
+++ Assets/scripts/FPSInput.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class FPSInput : MonoBehaviour
 {
     private float speed = 9.0f;
+    private CharacterController charController;
+    private float gravity = -9.8f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -14,7 +16,22 @@ public class FPSInput : MonoBehaviour
     {
         float horizinput = Input.GetAxis("Horizontal");
         float vertinput = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizinput, 0, vertinput) * Time.deltaTime * speed;
-        transform.Translate(movement);
+        Vector3 movement = new Vector3(horizinput, 0, vertinput);
+        // Clamp magnitude to limit diagonal movement
+        movement = Vector3.ClampMagnitude(movement, 1.0f);
+
+        // take speed into account
+        movement *= speed;
+
+        movement.y = gravity;
+        // make movement processor independent
+        movement *= Time.deltaTime;
+
+        // Convert local to global coordinates
+        movement = transform.TransformDirection(movement);
+
+        charController.Move(movement);
+
+        //transform.Translate(movement);
     }
 }
