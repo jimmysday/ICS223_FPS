36ee57ef4b37c90f1d584c718782605fd9f81d64
jimmysday <jaminzhang99@gmail.com>
2025-01-31 22:47:35 -0800

    task4 (lab 4)completed

--- Assets/scripts/ReactiveTarget.cs
+++ Assets/scripts/ReactiveTarget.cs
@@ -3,17 +3,6 @@ using UnityEngine;
 
 public class ReactiveTarget : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void ReactToHit()
     {
