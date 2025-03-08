93f2f6621a99302fb3d924bc3130482f361199ce
jimmysday <jaminzhang99@gmail.com>
2025-03-08 07:47:54 -0800

    compeled the task 3b

--- Assets/scripts/Rayshooter.cs
+++ Assets/scripts/Rayshooter.cs
@@ -51,13 +51,13 @@ public class ray : MonoBehaviour
         Destroy(sphere);
     }
 
-    void OnGUI()
-    {
-        GUIStyle style = new GUIStyle();
-        style.fontSize = aimSize;
-        // find the center of the camera view and adjust for asterisk
-        float posX = cam.pixelWidth / 2 - aimSize / 4;
-        float posY = cam.pixelHeight / 2 - aimSize / 2;
-        GUI.Label(new Rect(posX, posY, aimSize, aimSize), "*", style);
-    }
+    //void OnGUI()
+    //{
+    //    GUIStyle style = new GUIStyle();
+    //    style.fontSize = aimSize;
+    //    // find the center of the camera view and adjust for asterisk
+    //    float posX = cam.pixelWidth / 2 - aimSize / 4;
+    //    float posY = cam.pixelHeight / 2 - aimSize / 2;
+    //    GUI.Label(new Rect(posX, posY, aimSize, aimSize), "*", style);
+    //}
 }
